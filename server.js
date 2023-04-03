52278bd7395d01a505cd28fe600126ff09bc9834
Danny <perez6736@gmail.com>
2023-04-03 22:24:25 +0900

    use build on production but public locally

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var cors = require("cors");
-const path = require("path");
 
 var app = express();
-const router = express.Router();
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 3001;
@@ -24,7 +22,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // need this to use the js file and css file for the html pages.
-app.use(express.static(__dirname + "/client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/client/build"));
+} else {
+  app.use(express.static(__dirname + "/client/public"));
+}
 
 //CORS - react runs on a different port
 app.use(cors());

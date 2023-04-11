a6804bb0625b51e14c054f376a667b333fb2602d
Danny <perez6736@gmail.com>
2023-04-11 18:15:28 +0900

    oops i fix a mistake

--- client/src/api/kanji.js
+++ client/src/api/kanji.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-let url = process.env.REACT_APP_BASE_URL;
+let url;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   url = "http://localhost:3001/api/";
 } else {
-  url = REACT_APP_BASE_URL + "/api";
+  url = process.env.REACT_APP_BASE_URL + "/api";
 }
 
 export default axios.create({
-  baseURL: url,
+  baseURL: "http://localhost:3001/api/",
 });
